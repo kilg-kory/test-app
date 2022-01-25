3dd0cd60b3c03ca2348e6ea868630f032998732d
Кравцов Илья Георгиевич <IGKravtsov@sberbank.ru>
2022-01-25 13:09:46 +0300

    test suite todo case

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {AppBar, Box, Button, Container, TextField, Toolbar, Typography} from "@mui/material";
+import {useState} from "react";
+import ClearIcon from '@mui/icons-material/Clear';
+
+export const TaskList = ({handleDel, list}) => {
+    return (
+        <div style={{width: 350}}>
+            {list.map(task => <Box key={task} sx={{
+                marginTop: 1,
+                marginBottom: 1,
+                border: '1px dashed #ececec',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+            }}>
+
+                <div style={{marginLeft: 2}}>{task}</div>
+                <div><a onClick={() => handleDel(task)}><ClearIcon style={{color: '#b9b9b9'}}/></a></div>
+
+            </Box>)}
+        </div>
+    );
+};
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [taskList, setTaskList] = useState([])
+
+    const handleAddClick = () => {
+        //TODO add click
+    };
+
+    const handleDelClick = (del) => {
+        //TODO delete click
+    }
+    return (
+        <Container sx={{display: 'flex', justifyContent: 'center'}}>
+            <Box sx={{boxShadow: 1, width: 350, height: '80vh', overflow: 'scroll'}}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="div">
+                            TO DO
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <div style={{margin: 5}}>
+                    <TextField multiline sx={{width: 340}} rows={6}/>
+                </div>
+                <div>
+                    <Button color={"primary"} onClick={handleAddClick}>add</Button>
+                </div>
+                <div>
+                    <TaskList handleDel={handleDelClick} list={taskList}/>
+                </div>
+            </Box>
+        </Container>
+    );
 }
 
 export default App;
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,21 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom';
+// eslint-disable-next-line
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import TestRenderer from 'react-test-renderer'; // ES6
+
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.create = TestRenderer.create;
+// global.toJson = toJson;
+// console.error = (message) => {
+//   throw new Error(message);
+// };
